653097098a4cb372ed631f6af64e190c14ae1c42
sa02045 <sa02045@naver.com>
2023-10-24 23:50:33 +0900

    piece abstarct class

--- src/Piece.ts
+++ src/Piece.ts
@@ -1,1 +1,14 @@
-export abstract class Piece {}
+import { Position } from "./Position";
+import { Color } from "./Color";
+
+export abstract class Piece {
+  position: Position;
+  color: Color;
+  
+  constructor(position: Position, color: Color) {
+    this.position = position;
+    this.color = color;
+  }
+
+  abstract moves(): Position[];
+}
